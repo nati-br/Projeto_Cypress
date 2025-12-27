12f4ece6a595c29c9a786a6ca10315e863e8c1f1
Natália <48138054+nati-br@users.noreply.github.com>
2025-12-27 16:07:37 -0300

    Adicionando mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportTitle: 'Resultados',
+      reportPageTitle: 'Teste | Resultados'
+    },
     defaultCommandTimeout: 5000,
     baseUrl:"https://automationpratice.com.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
